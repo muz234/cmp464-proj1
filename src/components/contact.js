953fd36b90cdc1c7670d28067aa028940d963753
muzaffar ahmed <nobin_probin@hotmail.com>
2020-12-18 00:50:30 -0500

    first commit

---
+++ src/components/contact.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
+
+class Contact extends Component {
+  render() {
+    return(
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Muzaffar Ahmed</h2>
+            <img
+              src="https://photos.app.goo.gl/Yp1rH1jmVN6RPBZP7"
+              alt="avatar"
+              style={{height: '250px'}}
+               />
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                    (646)824-6407
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                   mahmed.res@gmail.com 
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Contact;
