fa526685ba15807ab7b9efe93fa3bde1021ac531
muzaffar ahmed <nobin_probin@hotmail.com>
2021-01-17 19:08:02 -0500

    changing name on title

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -8,36 +8,31 @@ class Landing extends Component {
       <div style={{width: '100%', margin: 'auto'}}>
         <Grid className="landing-grid">
           <Cell col={12}>
-            <img
-              src="https://photos.app.goo.gl/Yp1rH1jmVN6RPBZP7"
-              alt="avatar"
-              className="avatar-img"
-              />
-
             <div className="banner-text">
-              <h1>Full Stack Web Developer</h1>
+                  <h1>Full Stack Web Developer</h1>
+                <hr/>
+              <p>HTML/CSS | Bootstrap | JavaScript | React | Java | SQL</p>
+            <div className="social-links">
+              {/* LinkedIn */}
+                    <a href="https://www.linkedin.com/in/muzaffar90/" rel="noopener noreferrer" target="_blank">
+                <i className="fa fa-linkedin-square" aria-hidden="true" />
+              </a>
+              {/* Github */}
+                    <a href="https://github.com/muz234" rel="noopener noreferrer" target="_blank">
+                <i className="fa fa-github-square" aria-hidden="true" />
+              </a>
 
-            <hr/>
-
-          <p>HTML/CSS | Bootstrap | JavaScript | React | Java | SQL</p>
-
-        <div className="social-links">
-
-          {/* LinkedIn */}
-                <a href="https://www.linkedin.com/in/muzaffar90/" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-linkedin-square" aria-hidden="true" />
-          </a>
-
-          {/* Github */}
-                <a href="https://github.com/muz234" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-github-square" aria-hidden="true" />
-          </a>
-
-        </div>
+            </div>
             </div>
           </Cell>
         </Grid>
+        <img
+          src="https://photos.app.goo.gl/Yp1rH1jmVN6RPBZP7"
+          alt="avatar"
+          className="avatar-img"
+        />
       </div>
+      
     )
   }
 }
